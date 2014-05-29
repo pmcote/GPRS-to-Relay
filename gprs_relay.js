f689bebc9d2221d24958539b597295f244a93d23
pmcote <pmcote1@gmail.com>
2014-05-29 15:08:45 -0700

    Fixed about half of Tims filed issues

--- gprs_relay.js
+++ gprs_relay.js
@@ -10,9 +10,13 @@ to activate any other module by SMS. Have fun!
 **********************************************/
 
 var tessel = require('tessel');
-var gprs_port = tessel.port('A');
-var gprs = require('gprs-sim900').use(gprs_port);
-var relay = require('relay-mono').use(tessel.port('C'));
+var gprslib = require('gprs-sim900')
+var relaylib = require('relay-mono')
+
+var gprs = gprslib.use(tessel.port['A']);
+var relay = relaylib.use(tessel.port['C']);
+
+var LOGS = true; // If you don't want logs, set to false.
 
 // Emit unsolicited messages beginning with. . .
 gprs.emitMe(['+']);
@@ -24,22 +28,24 @@ function trigger (callback) {
 		var SMSindex = dataSplit[1];
 		var mode = 0; // Zero marks message as read, One will not change message status
 
-		gprs.readSMS(SMSindex, mode, function(err, message) {
-			if (err) console.log(err);
+		gprs.readSMS(SMSindex, mode, function (err, message) {
+			if (err) {
+				return console.err();
+			};
 			sendingInfo = message[1] // message is an array with index 0: Command echo, 1: Message information (read state, soure number, date), 2: Message text, 3; 'OK' if successful
-			console.log('Message received!\nMessage from numeber: ' + message[1]);
-			console.log('Message to Tessel: ' + message[2]);
+			if (LOGS) console.log('Message received!\nMessage from numeber: ', message[1]);
+			if (LOGS) console.log('Message to Tessel: ', message[2]);
 
 			// Now we're going to set a trigger for the next module action
 			var trigger = /Tessel/;
 			var isTrigger = trigger.test(message[2]);
 			if (isTrigger) {
-				console.log('We can tell a module what to do');
+				if (LOGS) console.log('We can tell a module what to do');
 				// A function with what you would like the module to do
 				callback();
 			}
 			else {
-				console.log('That was not the trigger you set, but congrats on having such a popular Tessel.');
+				if (LOGS) console.log('That was not the trigger you set, but congrats on having such a popular Tessel.');
 			}
 		});
 	});
@@ -48,20 +54,15 @@ function trigger (callback) {
 // The cool thing you want your triggered module to do. In this case, turn something on or off with the relay module.
 function coolAction (){
 	relay.toggle(1, function (err) {
-		if (err) console.log('There was an error toggling relay device 1 :(') 
-		console.log('toggled');
+		if (err) return console.err() 
+		if (LOGS) console.log('toggled');
 	});
 };
 
 // // Initialize GPRS module
 gprs.on('ready', function() {
 	// Wait around 30 seconds for the GPRS modual to connect before sending Tessel a text.
-	console.log("GPRS is on and waiting for you to text a trigger for your module");
+	if (LOGS) console.log("GPRS is on and waiting for you to text a trigger for your module");
 	// Assuming relay module is ready 
 	trigger(coolAction);
 });
-
-
-
-
-
